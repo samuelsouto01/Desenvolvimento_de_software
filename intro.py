843469028f27138dc1b0d4dcea332f040c837444
samuelsouto01 <soutosamuel@hotmail.com>
2025-03-04 14:17:58 -0300

    Create intro.py
    Começando a compreender linguagens de Programação (Python), é melhor opção por ser de sintaxe simples e com alto uso em aprendizagem.

---
+++ intro.py
@@ -0,0 +1,12 @@
+#     introdução a linguagem Python
+#  conceitos básicos da linguagem
+#  Operações aritmeticas ( '+' => soma , '-' => subtração, '/' => divisão, '*' => multiplicação)
+#  Operações de comparação ('==' igualdade , '!=' => diferença , '>' => maior que, '<' => menor que)
+#                    OBS: podemos utilizar a igualdade junto com o maior que ou menor que. EX '>=' maior que ou igual.
+#  Tipos de dados: inteiros, floats (numeros com virgula, neste caso o ponto), Caracter 'a' ,Strings (cadeia de caracteres "Samuel"), Boolean (Verdadeiro ou falso "True")
+#  A linguagem Python não é altamente tipada, ou seja uma variavel (que varia ou não com o decorrer do programa) pode assumir diferentes tipos de dados ao decorrer da execução
+
+#            ||  Primeiros comandos com o python ||
+# função 'print', é uma palavra reservada para 'imprimir' um dado na tela, Ex:
+print(" Olá Mundo!")
+
